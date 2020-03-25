80c6b6012eb0f0acbe2937dd321e1b760a4f7022
mlimony <62614138+mlimony@users.noreply.github.com>
2020-03-25 18:41:02 +0200

    Optimal number of kits
    Function to find optimal number of kits in given situation.
    Docs.
    Changing test_ip expression to sample_id.

--- classes/corona_test_stat.py
+++ classes/corona_test_stat.py
@@ -6,20 +6,20 @@ from classes.corona_testing import CoronaTesting
 
 
 class CoronaTestStat:
-    # Unique identifiers for the methods to create test ids for subjects.
+    # Unique identifiers for the methods to create sample ids for subjects.
     ASC = 0
     DEC = 1
     RANDOM = 2
 
     def __init__(self, num_of_subjects: int = 0b11111111, num_of_test_cases: int = 1000,
-                 infection_percentage: float = 2, test_id_types: List = None):
+                 infection_percentage: float = 2, sample_id_types: List = None):
         self.num_of_subjects = num_of_subjects
         self.num_of_test_cases = num_of_test_cases
         self.infection_percentage = infection_percentage
-        self.test_id_types = test_id_types if test_id_types else [self.ASC]
-        self.num_of_test_ids = len(self.test_id_types)
+        self.sample_id_types = sample_id_types if sample_id_types else [self.ASC]
+        self.num_of_sample_ids = len(self.sample_id_types)
         self.all_corona_test_cases = []
-        self.num_of_test_kits_in_case = bin_len(num_of_subjects) * self.num_of_test_ids
+        self.num_of_test_kits_in_case = bin_len(num_of_subjects) * self.num_of_sample_ids
         self.running_time = 0
 
         self.all_infected = []
@@ -39,15 +39,15 @@ class CoronaTestStat:
             # Creating the test object and initialize the subject's names.
             test = CoronaTesting(self.num_of_subjects, test_name=f'{idx}')
             test.generate_subjects_by_num()
-            # Adding multiple test ids to each subject.
-            for test_id_type in self.test_id_types:
-                if test_id_type == self.ASC:
+            # Adding multiple sample ids to each subject.
+            for sample_id_type in self.sample_id_types:
+                if sample_id_type == self.ASC:
                     test.add_num_sample_id()
-                if test_id_type == self.DEC:
+                if sample_id_type == self.DEC:
                     test.add_num_sample_id(reverse=True)
-                if test_id_type == self.RANDOM:
+                if sample_id_type == self.RANDOM:
                     test.add_random_sample_id()
-            # Create dictionary with all the test ids of each subject.
+            # Create dictionary with all the sample ids of each subject.
             test.generate_subjects_dict()
             # Generates sick subject randomly.
             test.generate_infected(self.infection_percentage)
@@ -55,6 +55,8 @@ class CoronaTestStat:
             # having the generated infected subjects among them to make some of the test positive.
             test.generate_test_result()
             test.find_potential_positive()
+            # Narrow down the possible options for groups of subjects that matches the results.
+            # test.find_groups_that_match_result()
 
             # Updating the stats
             self.all_corona_test_cases.append(test)
@@ -73,17 +75,18 @@ class CoronaTestStat:
         print(f'Infection percentage in each case: {self.infection_percentage}%')
         print(f'Number of Covid-19 test kits used: {self.num_of_test_kits_in_case}')
         # Todo - change to names instead of numbers
-        self.show_test_id_types()
+        self.show_sample_id_types()
         print(f'Average real Covid-19 positive subjects in case: {self.avg_infected}')
         print(f'Average potential Covid-19 positive subjects found by the algorithm: {self.avg_potential_positive}')
+        print(f'---')
 
-    def show_test_id_types(self):
-        test_id_types_names = []
-        for test_id_type in self.test_id_types:
-            if test_id_type == self.ASC:
-                test_id_types_names.append('ASC')
-            if test_id_type == self.DEC:
-                test_id_types_names.append('DEC')
-            if test_id_type == self.RANDOM:
-                test_id_types_names.append('RANDOM')
-        print(f'Methods of creating test ids: {test_id_types_names}')
+    def show_sample_id_types(self):
+        sample_id_types_names = []
+        for sample_id_type in self.sample_id_types:
+            if sample_id_type == self.ASC:
+                sample_id_types_names.append('ASC')
+            if sample_id_type == self.DEC:
+                sample_id_types_names.append('DEC')
+            if sample_id_type == self.RANDOM:
+                sample_id_types_names.append('RANDOM')
+        print(f'Methods of creating sample ids: {sample_id_types_names}')
